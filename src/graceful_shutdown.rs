8df78af4eb546a1f564589d319ecbc73afb30343
0x9bb1 <zhangnix2@outlook.com>
2023-05-23 09:41:08 +0800

    fix windows build error

--- src/graceful_shutdown.rs
+++ src/graceful_shutdown.rs
@@ -1,4 +1,3 @@
-use tokio::signal::unix::SignalKind;
 use tracing::event;
 
 pub async fn shutdown_signal() {
@@ -10,7 +9,7 @@ pub async fn shutdown_signal() {
 
     #[cfg(unix)]
     let terminate = async {
-        tokio::signal::unix::signal(SignalKind::terminate())
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
             .expect("failed to install signal handler")
             .recv()
             .await;
